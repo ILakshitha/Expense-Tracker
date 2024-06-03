50728db7ea20e51ed8aa2a876dceeba82c09dc77
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-06-04 02:58:17 +0530

    add some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
 import './App.css';
+import Balance from "./components/Balance";
+import Income_Expences from "./components/Income_Expences";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <div className="container">
+      <Balance/>
+      <Income_Expences/>
+      <TransactionList/>
+     </div>
     </div>
   );
 }
