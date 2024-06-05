3e84cce18f4a1e524c8210db2b8e24ee5481c425
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-06-05 10:55:41 +0530

    Create SignUp api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutre from './routes/auth.route.js';
 
 
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 
@@ -20,3 +22,4 @@ app.listen(3000, ()=>{
 });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth', authRoutre);
